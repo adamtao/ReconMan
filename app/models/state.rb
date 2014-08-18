fec63525f39e038a2ffcf3f32d5d135ef7fa2db4
Adam Anderson <adam@makeascene.com>
2014-08-18 14:59:48 -0600

    Attempting inactive state scope fix

--- app/models/state.rb
+++ app/models/state.rb
@@ -7,7 +7,7 @@ class State < ActiveRecord::Base
 	after_initialize :load_defaults
 
 	scope :active, -> { where(active: true).order('UPPER(name)') }
-	scope :inactive, -> { where("active IS NULL OR active = 0").order('UPPER(name)') }
+	scope :inactive, -> { where(active: [0, nil, false]).order('UPPER(name)') }
 
 	def self.due_within_options
 		[30, 60, 90]
@@ -16,4 +16,5 @@ class State < ActiveRecord::Base
 	def load_defaults
 		self.due_within_days ||= self.class.due_within_options.first
 	end
+
 end
