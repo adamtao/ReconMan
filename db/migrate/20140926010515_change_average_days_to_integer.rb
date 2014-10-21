5b77915dbfce17edcf92c78df5cf7bfc3708c376
Adam Anderson <adam@makeascene.com>
2014-10-20 21:18:54 -0600

    Fixing migration for postgres

--- db/migrate/20140926010515_change_average_days_to_integer.rb
+++ db/migrate/20140926010515_change_average_days_to_integer.rb
@@ -1,5 +1,7 @@
 class ChangeAverageDaysToInteger < ActiveRecord::Migration
   def change
-    change_column :counties, :average_days_to_complete, :integer
+    remove_column :counties, :average_days_to_complete
+    add_column :counties, :average_days_to_complete, :integer
+
   end
 end
