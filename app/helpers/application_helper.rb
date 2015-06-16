716fcd914f9d437406ec7c41c09df4928d195630
Adam Anderson <adam@makeascene.com>
2015-06-16 09:38:21 -0600

    Updating gems, trying to show better errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def errors_for(f)
+    if f.object.errors.any?
+      content_tag(:ul, f.object.errors.full_messages.uniq.map{|msg| content_tag(:li, msg)}.join.html_safe )
+    end
+  end
+
 end
